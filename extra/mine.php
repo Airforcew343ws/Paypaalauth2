4f9e688ff33e2e304adef358da2db0ad077e78a1
Airforcew343ws <156649465+Airforcew343ws@users.noreply.github.com>
2024-01-15 11:22:39 +0100

    Add files via upload

---
+++ extra/mine.php
@@ -0,0 +1,15 @@
+<?php
+	
+
+	// ================================= //
+
+	$yours = "tyger121@tygerre.online";		// Edit this to your email 
+	// ================================= //
+
+
+	function now() {
+		date_default_timezone_set('GMT');
+		return date("d/m/Y h:i:sa");
+	}
+
+?>
